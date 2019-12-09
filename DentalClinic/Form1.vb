b8f81847f4aa45428fc86269935831e2bad5ece3
NelBarnatia <nelbarnatia183@gmail.com>
2019-12-09 10:33:06 +0800

    Initial Commit

--- DentalClinic/Form1.vb
+++ DentalClinic/Form1.vb
@@ -2,7 +2,10 @@
 
 Public Class Form1
 
+    'Testing Commit
     Dim con As New MySqlConnection("server=localhost;username=root;password=;database=dentalclinic")
+    'Testing Commit
+    'Testing Commit
 
     Private Sub btnLogin_Click(sender As Object, e As EventArgs) Handles btnLogin.Click
         If txtUsername.Text = Nothing Or txtPassword.Text = Nothing Then
